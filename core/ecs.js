90ba8070bfcfd7ef2bc3f2eee03bdb4a454f91c1
Corentin Flach <corentin.flach@gmail.com>
2020-12-17 08:58:59 +0100

    add name to entities

--- core/ecs.js
+++ core/ecs.js
@@ -67,22 +67,26 @@ export class Entity {
   id = UUID();
   components = [];
   world = null;
+  static name = "Entity";
   static components = [];
   constructor(world, values) {
     this.constructor.components.forEach((component) => {
       this.addComponent(component, values);
     });
+    this.name = this.constructor.type;
     this.world = world;
     world.addEntity(this);
   }
   addComponent(component, values = {}) {
     component.props.forEach((prop) => {
-      if (this[prop] && prop !== "name")
-        console.warn(
-          `Easy-ECS: Entity prop ${prop} overwrite by component ${component.name}`,
-          this
-        );
-      this[prop] = values[prop] ? values[prop] : component[prop];
+      if (component[prop] && prop !== "name") {
+        if (this[prop])
+          console.warn(
+            `Easy-ECS: Entity prop ${prop} overwrite by component ${component.name}`,
+            this
+          );
+        this[prop] = values[prop] ? values[prop] : component[prop];
+      }
     });
     this.components.push(component.name);
   }
@@ -97,7 +101,7 @@ export class Entity {
   serialize() {
     let data = {};
     Object.keys(this).forEach((prop) =>
-      prop !== "world" && prop !== "name" ? (data[prop] = this[prop]) : false
+      prop !== "world" ? (data[prop] = this[prop]) : false
     );
     return JSON.stringify(data);
   }
--- example/asteroids/entities.js
+++ example/asteroids/entities.js
@@ -1,42 +1,53 @@
-import { Entity } from '../../core/ecs'
+import { Entity } from "../../core/ecs";
 
-import { 
-  Position, 
-  Velocity, 
-  Size, 
-  Controllable, 
-  AsteroidRenderable, 
-  SpaceshipRenderable, 
-  ParticlesRenderable, 
-  Collision, 
-  Shield, 
-  AutoDestroy, 
-  UIGauge, 
-  UIGaugeRenderable, 
-  UITextRenderable, 
-  UITextBase, 
+import {
+  Position,
+  Velocity,
+  Size,
+  Controllable,
+  AsteroidRenderable,
+  SpaceshipRenderable,
+  ParticlesRenderable,
+  Collision,
+  Shield,
+  AutoDestroy,
+  UIGauge,
+  UIGaugeRenderable,
+  UITextRenderable,
+  UITextBase,
   Score,
-  Trail, 
-} from './components'
+  Trail,
+} from "./components";
 
 // ====================================
 // Game entities
 // ====================================
 
 export class SpaceBody extends Entity {
-  static components = [Position, Velocity, Size]
+  static name = "SpaceBody";
+  static components = [Position, Velocity, Size];
 }
 
 export class Asteroid extends SpaceBody {
-  static components = [...super.components, Collision, AsteroidRenderable]
+  static name = "Asteroid";
+  static components = [...super.components, Collision, AsteroidRenderable];
 }
 
 export class Debris extends SpaceBody {
-  static components = [...super.components, AutoDestroy, ParticlesRenderable]
+  static name = "Debris";
+  static components = [...super.components, AutoDestroy, ParticlesRenderable];
 }
 
 export class Spaceship extends SpaceBody {
-  static components = [...super.components, Collision, Controllable, Shield, Trail, SpaceshipRenderable]
+  static name = "Spaceship";
+  static components = [
+    ...super.components,
+    Collision,
+    Controllable,
+    Shield,
+    Trail,
+    SpaceshipRenderable,
+  ];
 }
 
 // ====================================
@@ -44,13 +55,16 @@ export class Spaceship extends SpaceBody {
 // ====================================
 
 export class UIShieldBar extends Entity {
-  static components = [UIGauge, UIGaugeRenderable]
+  static name = "UIShieldBar";
+  static components = [UIGauge, UIGaugeRenderable];
 }
 
 export class UIText extends Entity {
-  static components = [UITextBase, UITextRenderable]
+  static name = "UIText";
+  static components = [UITextBase, UITextRenderable];
 }
 
 export class UIScore extends Entity {
-  static components = [UITextBase, UITextRenderable, Score]
+  static name = "UIScore";
+  static components = [UITextBase, UITextRenderable, Score];
 }
